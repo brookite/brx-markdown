3c30d631567b9d22b8728186b03a5144a15ab541
brookit <brookit18@gmail.com>
2023-12-30 23:35:55 +0300

    Added wikiimages and highlights

--- brxmd.py
+++ brxmd.py
@@ -9,8 +9,110 @@ from mdit_py_plugins.tasklists import tasklists_plugin
 from mdit_py_plugins.field_list import fieldlist_plugin
 
 
+def _wikiimage_inline(state, silent: bool):
+    if state.src[state.pos] != "!" or state.src[state.pos + 1] != "[" or state.src[state.pos + 2] != "[":
+        return False
+    try:
+        end = state.src.index("]]", state.pos + 3)
+    except ValueError:
+        return False
+
+    text = state.src[state.pos + 3 : end]
+    if not text.strip():
+        return False
+
+    try:
+        index = text.index("|")
+        link = text[:index]
+        res = text[index + 1 :]
+        if not res:
+            width, height = None, None
+        else:
+            res = res.split("x")
+            if len(res) == 2:
+                width, height = res
+            else:
+                width, height = None, None
+    except ValueError:
+        link = text
+        width, height = None, None
+
+    if not silent:
+        token = state.push("wikiimage", "wiki", 0)
+        token.content = link
+        token.attrSet("width", width)
+        token.attrSet("height", height)
+
+    state.pos = end + 2
+    return True
+
+
+def wikiimage_plugin(md):
+    md.inline.ruler.before(
+        "escape",
+        "wikiimage",
+        _wikiimage_inline,
+    )
+
+    def render_wikiimage(self, tokens, idx, options, env) -> str:
+        src = str(tokens[idx].content)
+
+        width = tokens[idx].attrGet("width")
+        height = tokens[idx].attrGet("height")
+        if str(width).isdigit():
+            width = str(width) + "px"
+        if str(height).isdigit():
+            height = str(height) + "px"
+
+        width = 'width="{}" '.format(width)
+        height = 'height="{}"'.format(height)
+        args = ''
+        if tokens[idx].attrGet("width"):
+            args += width
+        if tokens[idx].attrGet("height"):
+            args += height
+        return f'<img class="wikiimage" src="{src}" {args}></img>'
+
+    md.add_render_rule("wikiimage", render_wikiimage)
+
+
+def _highlights_inline(state, silent: bool):
+    if state.src[state.pos] != "=" or state.src[state.pos + 1] != "=":
+        return False
+    try:
+        end = state.src.index("==", state.pos + 2)
+    except ValueError:
+        return False
+
+    text = state.src[state.pos + 2 : end]
+    if not text.strip():
+        return False
+
+    if not silent:
+        token = state.push("highlight", "highlight", 0)
+        token.content = text
+
+    state.pos = end + 2
+    return True
+
+
+def highlights_plugin(md):
+    md.inline.ruler.before(
+        "escape",
+        "highlight",
+        _highlights_inline,
+    )
+
+    def render_highlight(self, tokens, idx, options, env) -> str:
+        label = str(tokens[idx].content)
+        return f'<mark>{label}</mark>'
+
+    md.add_render_rule("highlight", render_highlight)
+
+
+
 def _wikilinks_inline(state, silent: bool):
-    if state.src[state.pos] != "[" or state.src[state.pos] != "[":
+    if state.src[state.pos] != "[" or state.src[state.pos + 1] != "[":
         return False
     try:
         end = state.src.index("]]", state.pos + 2)
@@ -62,6 +164,8 @@ def brx_markdown(html_support=True, newline_break=True):
         .use(tasklists_plugin)
         .use(fieldlist_plugin)
         .use(wikilinks_plugin)
+        .use(wikiimage_plugin)
+        .use(highlights_plugin)
     )
 
 
